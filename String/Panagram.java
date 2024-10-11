3b48769cde81ae7ccc1143688672d0ad12883c6b
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2024-10-11 23:37:03 +0530

    String

---
+++ String/Panagram.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Panagram{
+    public static void main(String[] args){
+        String s1 = "THE QUICK BRWON FOX JUMPS OVER THE LAZY DOG";
+        s1 = s1.replace(" ","");
+        s1 = Arrays.sort(s1);
+
+        int arr[] = new int[25];
+
+        for(int i=0;i<arr.lnegth;i++){
+
+        }
+    }
+}
