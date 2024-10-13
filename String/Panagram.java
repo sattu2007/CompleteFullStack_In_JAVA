e602970f8504b7e9293486079582cf9bc2081742
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2024-10-13 14:31:56 +0530

    Panagram

--- String/Panagram.java
+++ String/Panagram.java
@@ -5,12 +5,24 @@ public class Panagram{
     public static void main(String[] args){
         String s1 = "THE QUICK BRWON FOX JUMPS OVER THE LAZY DOG";
         s1 = s1.replace(" ","");
-        s1 = Arrays.sort(s1);
+        char [] ch = s1.toCharArray();
 
-        int arr[] = new int[25];
+        int arr[] = new int[26];
+        boolean hyder = false;
+        for(int i=0;i<ch.length;i++){
+            int index = ch[i]-65;
+            arr[index]++;
+            //ar[ch[i]-65]++;
+        }
 
-        for(int i=0;i<arr.lnegth;i++){
-
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                System.out.println("It is not panagram");
+                hyder =true;
+            }
+        }
+        if(hyder == false){
+            System.out.println("It is panagram");
         }
     }
 }
